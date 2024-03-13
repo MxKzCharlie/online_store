c95260b50594a428bb55d7abc74a8d28329fcda1
MxKzCharlie <163343038+MxKzCharlie@users.noreply.github.com>
2024-03-13 12:55:16 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-const imagen = document.getElementById('imagen');
-const imagenes = [
-    'Imagenes/cat_Charlie.png',
-    'Imagenes/Cat_Michell.png',
-    'Imagenes/Cat2_Michi.png',
-    'Imagenes/CatTravieso_Michi.png'
-]
-let index = 0;
+let cantidadProductos = 0;
 
-function testImg() {
-    index++;
-    if (index <= imagenes.length - 1){
-        imagen.src = imagenes[index];
-    }else{
-        index = 0;
-        imagen.src = imagenes[index];
-    }
+function agregarAlCarrito (producto){
+    let carrito = document.getElementById('carrito');
+    carrito.style.display = 'flex';
+
+    cantidadProductos += producto;
+    let span = document.getElementById('cantidad_productos');
+    span.textContent = cantidadProductos.toString();
 }
