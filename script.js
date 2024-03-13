616b8dbd5c12601129a36bd8cf1773f1c7e662d5
MxKzCharlie <163343038+MxKzCharlie@users.noreply.github.com>
2024-03-13 12:52:06 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let cantidadProductos = 0;
+const imagen = document.getElementById('imagen');
+const imagenes = [
+    'Imagenes/cat_Charlie.png',
+    'Imagenes/Cat_Michell.png',
+    'Imagenes/Cat2_Michi.png',
+    'Imagenes/CatTravieso_Michi.png'
+]
+let index = 0;
 
-function agregarAlCarrito (producto){
-    let carrito = document.getElementById('carrito');
-    carrito.style.display = 'flex';
-
-    cantidadProductos += producto;
-    let span = document.getElementById('cantidad_productos');
-    span.textContent = cantidadProductos.toString();
+function testImg() {
+    index++;
+    if (index <= imagenes.length - 1){
+        imagen.src = imagenes[index];
+    }else{
+        index = 0;
+        imagen.src = imagenes[index];
+    }
 }
